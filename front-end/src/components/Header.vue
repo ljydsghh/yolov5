0a4d133197784465fa0af575ebc9738a21fb3682
lujinyang2023 <3431806923@qq.com>
2024-06-27 10:59:37 +0800

    change

---
+++ front-end/src/components/Header.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="Header">
+    <div class="top-left-edition">
+      <span style="color: #21b3b9; font-weight: bold">
+        <i class="el-icon-star-off" style="font-size: 23px"></i
+        >武汉智矿三维有限公司
+      </span>
+      <span>
+        <i class="el-icon-time" style="font-size: 23px"></i>制作者：路金洋
+      </span>
+    </div>
+    <div id="word">
+      <h1>{{ msg }}</h1>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      msg: "矿产图像智能识别",
+      activeIndex: "1",
+    };
+  },
+  methods: {},
+};
+</script>
+<style scoped>
+#Header {
+  padding: 30px 110px 0 150px;
+  width: 90%;
+  margin: 10px auto;
+}
+
+#word {
+  margin-left: 45%;
+  margin-top: -35px;
+  margin-bottom: 37px;
+  height: 60px;
+  line-height: 3.2em;
+  letter-spacing: 8px;
+}
+
+h1 {
+  /*text-align: center;*/
+  color: #21b3b9;
+  letter-spacing: 30px;
+  font-size: 2.3em;
+}
+
+.el-menu-demo {
+  width: 80%;
+  margin: 0px auto;
+  padding: 0px auto;
+}
+
+.top-left-edition span i {
+  float: left;
+  margin-right: 10px;
+}
+
+i,
+input,
+label {
+  vertical-align: middle;
+}
+
+i {
+  border: 0;
+  display: block;
+  cursor: pointer;
+}
+
+.top-left-edition span {
+  float: left;
+  font-size: 16px;
+  color: #999999;
+  line-height: 24px;
+  margin-right: 40px;
+}
+</style>
+
+
